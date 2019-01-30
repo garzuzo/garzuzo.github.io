07ae289717ab16b6f355968ca2d4a03a1de0bdef
Johnatan Garzón <garzuxo@gmail.com>
2019-01-30 15:35:07 -0500

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,13 @@
+$("#show-hide-empl").on("click",function(event){
+
+  //  $(this).style.visibility="hidden";
+$("#hide-empl").fadeToggle();
+});
+
+
+$("#show-hide-edu").on("click",function(event){
+
+  //  $(this).style.visibility="hidden";
+  $("#hide-edu").fadeToggle();
+
+});
